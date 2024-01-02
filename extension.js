fe1f6695d008d4633d9d71db36097491b755bb32
David Scholberg <recombinant.vector@gmail.com>
2024-01-02 14:23:45 -0500

    fixed up window attention handling

--- extension.js
+++ extension.js
@@ -1,32 +1,52 @@
+import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
+import GObject from "gi://GObject";
+import * as UIMain from "resource:///org/gnome/shell/ui/main.js";
 
-// import GObject from 'gi://GObject';
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
-// import * as UIMain from 'resource:///org/gnome/shell/ui/main.js';
-
+// Main extension class. All modifications should be enabled and disabled here.
 export default class GuhNomeEnjoyment extends Extension {
-    enable() {
-    }
+  enable() {
+    windowDemandsAttentionFocusEnable();
+  }
 
-    disable() {
-    }
+  disable() {
+    windowDemandsAttentionFocusDisable();
+  }
 }
 
-// const replaceSignalHandler = (signalName, newHandler) => {
-//     global.display.connect(signalName, newHandler);
+// Replace current signal handler with a new one. This function currently
+// assumes that only one signal handler is installed for the given signal.
+// The new handler should be of the form (display, gobject) => {}.
+const replaceSignalHandler = (signalName, newHandler) => {
+  const oldSignalID = GObject.signal_handler_find(global.display, {
+    signalId: signalName,
+  });
+  global.display.disconnect(oldSignalID);
 
-//     const oldSignalID = GObject.signal_handler_find(global.display, {signalId: signalName});
-//     global.display.disconnect(oldSignalID);
-// }
+  global.display.connect(signalName, newHandler);
+};
 
-// const windowDemandsAttentionFocusEnable = () => {
-//     const focusHandler = (display, window) => {
-//         if (!window || window.has_focus() || window.is_skip_taskbar()) {
-//             return;
-//         }
+// Restore the default signal handlers for the "window-demands-attention" and
+// "window-marked-urgent" signals, which just sends a notification.
+const windowDemandsAttentionFocusDisable = () => {
+  const wah = UIMain.windowAttentionHandler;
+  const originalHandler = wah._onWindowDemandsAttention.bind(wah);
 
-//         UIMain.activateWindow(window);
-//     };
+  replaceSignalHandler("window-demands-attention", originalHandler);
+  replaceSignalHandler("window-marked-urgent", originalHandler);
+};
 
-//     replaceSignalHandler('window-demands-attention', focusHandler);
-//     replaceSignalHandler('window-marked-urgent', focusHandler);
-// }
+// Update the "window-demands-attention" and "window-marked-urgent" signal
+// handlers to focus the window instead of the default behavior of just sending
+// a notification.
+const windowDemandsAttentionFocusEnable = () => {
+  const focusHandler = (display, window) => {
+    if (!window || window.has_focus() || window.is_skip_taskbar()) {
+      return;
+    }
+
+    UIMain.activateWindow(window);
+  };
+
+  replaceSignalHandler("window-demands-attention", focusHandler);
+  replaceSignalHandler("window-marked-urgent", focusHandler);
+};
